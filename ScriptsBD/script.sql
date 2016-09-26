3c983cef06dd423b19aabbfcdd1376e86cf17542
calopezf <calopezf123@gmail.com>
2016-09-26 00:29:54 -0500

    Insert Usuarios con Roles

--- ScriptsBD/script.sql
+++ ScriptsBD/script.sql
@@ -1,7 +1,13 @@
-select lalalalala
+INSERT INTO public.rol(
+            id, descripcion, estado)
+    VALUES ('ADMINISTRADOR', 'Administrador del Sistema', 'ACT');
+INSERT INTO public.rol(
+            id, descripcion, estado)
+    VALUES ('MEDICO', 'Medico', 'ACT');
+INSERT INTO public.usuario(
+            identificacion, apellido, direccion, email, estado, nombre, password)
+    VALUES ('1718891052', 'LOPEZ', '12 de octubre', 'calopezf@gmail.com', 'ACT', 'CRISTIAN', '2f2e70c7b5ee74ca8f2250a11de7b277');
+INSERT INTO public.usuario_rol(
+            rol_id, usuario_identificacion)
+    VALUES ('ADMINISTRADOR','1718891052');
 
-select otra cosa
-
-select * from usuario
-
-select * from mula
